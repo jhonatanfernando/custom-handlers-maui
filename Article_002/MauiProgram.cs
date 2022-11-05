566d8676166dce0bf2fed86ba308829417ee40df
Jhonatan Oliveira <jhonatan.oliveira@anm.gov.br>
2022-11-04 20:56:00 -0400

    Project Created

---
+++ Article_002/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Article_002.Controls;
+
+namespace Article_002;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+                    .ConfigureMauiHandlers((handlers) => {
+#if ANDROID
+                        handlers.AddHandler(typeof(ImageEntry), typeof(Platforms.Android.Renderes.ImageEntryRenderer));
+
+#elif IOS                    
+                        handlers.AddHandler(typeof(ImageEntry), typeof(Platforms.iOS.Renderes.ImageEntryRenderer));
+#endif
+                    });
+
+        return builder.Build();
+	}
+}
---
+++ Article_002/Platforms/iOS/Renderes/ImageEntryRenderer.cs
@@ -0,0 +1,51 @@
+﻿using Article_002.Controls;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using System.Drawing;
+using UIKit;
+
+namespace Article_002.Platforms.iOS.Renderes
+{
+    public class ImageEntryRenderer : EntryHandler
+    {
+        private ImageEntry element;
+
+        protected override MauiTextField CreatePlatformView()
+        {
+            var textField = new MauiTextField();
+            element = VirtualView as ImageEntry;
+
+            if (!string.IsNullOrEmpty(element.Image))
+            {
+                switch (element.ImageAlignment)
+                {
+                    case ImageAlignment.Left:
+                        textField.LeftViewMode = UITextFieldViewMode.Always;
+                        textField.LeftView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
+                        break;
+                    case ImageAlignment.Right:
+                        textField.RightViewMode = UITextFieldViewMode.Always;
+                        textField.RightView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
+                        break;
+                }
+            }
+
+            textField.BorderStyle = UITextBorderStyle.Line;
+            textField.Layer.MasksToBounds = true;
+
+            return textField;
+        }
+
+        private UIView GetImageView(string imagePath, int height, int width)
+        {
+            var uiImageView = new UIImageView(UIImage.FromFile(imagePath))
+            {
+                Frame = new RectangleF(0, 0, width, height)
+            };
+            UIView objLeftView = new UIView(new System.Drawing.Rectangle(0, 0, width + 10, height));
+            objLeftView.AddSubview(uiImageView);
+
+            return objLeftView;
+        }
+    }
+}
